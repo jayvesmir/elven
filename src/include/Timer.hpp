6294c4af0123beb18308a7dc3e50c9091c28b59c
Marcel <marceleenah@gmail.com>
2023-01-21 22:05:43 +0100

    Enable compiler optimizations

--- src/include/Timer.hpp
+++ src/include/Timer.hpp
@@ -4,7 +4,11 @@ using namespace std::chrono;
 
 class Timer {
 public:
+#ifdef _WIN32
+    std::chrono::steady_clock::time_point start, end;
+#else
     std::chrono::time_point<system_clock> start, end;
+#endif
     std::chrono::duration<float> duration;
     const char* title;
 
