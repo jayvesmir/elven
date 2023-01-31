409dd57a6591be7986d6dec648b69a82f4060b9d
Marcel <marceleenah@gmail.com>
2023-01-31 19:25:51 +0100

    Semicolon :(

--- src/Ayanami.cpp
+++ src/Ayanami.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
     Serializer::SceneSerializer serializer;
-    if (!serializer.load_scene("sample_scene.elv"));
+    if (!serializer.load_scene("sample_scene.elv"))
         exit(-1);
     Scene* scene = serializer.get_scene();
     scene->render();
